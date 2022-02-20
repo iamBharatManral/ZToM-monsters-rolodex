08358bae02c4b97237486a7bfb5fecbd00ebc000
bharatmanral <bharat.manral@airfi.aero>
2022-02-20 19:47:09 +0530

    Converted function component to class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-        Hello From React
-    </div>
-  );
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      string: "Hello From React Using State"
+    }
+  }
+  render() {
+    return (
+		<div>
+        <h2>{this.state.string}</h2>
+        <button onClick={ () => this.setState({string : "New text"})}>Change text</button>
+		</div>
+	);
+  }
 }
-
 export default App;
